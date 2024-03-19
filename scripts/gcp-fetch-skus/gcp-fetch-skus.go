c049b51a4c9f06a4dafd537920bd6a982aa85e1d
pokom <mark.poko@grafana.com>
2024-03-19 14:15:08 -0400

    refactor gcp-fetch-skus to have some semblence of reusability

--- scripts/gcp-fetch-skus/gcp-fetch-skus.go
+++ scripts/gcp-fetch-skus/gcp-fetch-skus.go
@@ -1,8 +1,11 @@
+// Usage: go run gcp-fetch-skus.go
+// THis is a useful utility if you want to fetch a set of sku's for a particular service and export them to CSV.
 package main
 
 import (
 	"context"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -13,19 +16,35 @@ import (
 	"github.com/grafana/cloudcost-exporter/pkg/google/billing"
 )
 
+type Config struct {
+	Service    string
+	OutputFile string
+}
+
 func main() {
+	var config *Config
+	flag.StringVar(&config.Service, "service", "Compute Engine", "The service to fetch skus for")
+	flag.StringVar(&config.OutputFile, "output-file", "skus.csv", "The file to write the skus to")
+	flag.Parse()
+	if err := run(config); err != nil {
+		log.Printf("error: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run(config *Config) error {
 	ctx := context.Background()
 	client, err := billingv1.NewCloudCatalogClient(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Close()
-	svcid, err := billing.GetServiceName(ctx, client, "Compute Engine")
+	svcid, err := billing.GetServiceName(ctx, client, config.Service)
 	if err != nil {
 		log.Fatal(err)
 	}
 	skus := billing.GetPricing(ctx, client, svcid)
-	file, err := os.Create("skus-with-license.csv")
+	file, err := os.Create(config.OutputFile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,8 +52,7 @@ func main() {
 	writer := csv.NewWriter(file)
 	err = writer.Write([]string{"sku_id", "description", "category", "region", "pricing_info"})
 	if err != nil {
-		log.Printf("error writing record to csv: %v", err)
-		os.Exit(1)
+		return fmt.Errorf("error writing record to csv: %v", err)
 	}
 	skusWithMultipleRates := map[string]int{}
 	for _, sku := range skus {
@@ -55,13 +73,10 @@ func main() {
 			}
 			err = writer.Write([]string{sku.SkuId, sku.Description, sku.Category.ResourceFamily, region, price})
 			if err != nil {
-				log.Printf("error writing record to csv: %v", err)
-				os.Exit(1)
+				return fmt.Errorf("error writing record to csv: %v", err)
 			}
 		}
 	}
 	writer.Flush()
-	if writer.Error() != nil {
-		log.Fatal(writer.Error())
-	}
+	return writer.Error()
 }
