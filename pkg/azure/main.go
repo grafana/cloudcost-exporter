eba55dfef8ac89533186a1fc952ff8fedcb33f7d
Logan Ballard <loganballard@gmail.com>
2024-06-20 17:45:15 -0400

    getting total cost

--- pkg/azure/main.go
+++ pkg/azure/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v4"
@@ -21,6 +22,7 @@ type PricingDetails struct {
 	SkuId         string  `json:"skuId"`
 	Type          string  `json:"type"`
 	ServiceFamily string  `json:"serviceFamily"`
+	ArmSkuName    string  `json:"armSkuName"`
 }
 
 type PricingApiResponse struct {
@@ -28,7 +30,14 @@ type PricingApiResponse struct {
 	NextPageLink string           `json:"NextPageLink"`
 }
 
-type PriceMap map[string]PricingDetails
+type VmScaleSetInfo struct {
+	Name     string
+	Capacity int
+	TypeSku  string
+	Location string
+}
+
+type PriceMap map[string]map[string]PricingDetails
 
 var computeClientFactory *armcompute.ClientFactory
 var costClientFactory *armcostmanagement.ClientFactory
@@ -41,12 +50,22 @@ func parseResponse(p *PricingApiResponse, pm *PriceMap) {
 		if price.ServiceFamily != "Compute" {
 			continue
 		}
-		(*pm)[price.SkuId] = price
+		if _, ok := (*pm)[price.Region]; !ok {
+			(*pm)[price.Region] = map[string]PricingDetails{}
+		}
+		(*pm)[price.Region][price.ArmSkuName] = price
 	}
 }
 
-func generatePricingMapFromApi() {
-	apiUrl := "https://prices.azure.com/api/retail/prices?api-version=2023-01-01-preview&$filter=serviceFamily eq 'Compute' and priceType eq 'Consumption'"
+func generatePricingMapFromApi(machineTypes []string) PriceMap {
+	machineTypesStringFilters := []string{}
+
+	for _, t := range machineTypes {
+		machineTypesStringFilters = append(machineTypesStringFilters, fmt.Sprintf("armSkuName eq '%s'", t))
+	}
+	queryFilterString := strings.Join(machineTypesStringFilters, " or ")
+
+	apiUrl := fmt.Sprintf("https://prices.azure.com/api/retail/prices?api-version=2023-01-01-preview&$filter=serviceFamily eq 'Compute' and priceType eq 'Consumption' and %s", queryFilterString)
 	priceMap := PriceMap{}
 
 	for {
@@ -75,7 +94,7 @@ func generatePricingMapFromApi() {
 		apiUrl = result.NextPageLink
 	}
 
-	fmt.Println(priceMap)
+	return priceMap
 }
 
 func main() {
@@ -100,6 +119,8 @@ func main() {
 	opts := armcompute.VirtualMachineScaleSetsClientListAllOptions{}
 	pager := virtualMachineScaleSetsClient.NewListAllPager(&opts)
 
+	vmScaleSetInfoMap := map[string]VmScaleSetInfo{}
+
 	for pager.More() {
 		nextResult, err := pager.NextPage(ctx)
 		if err != nil {
@@ -107,10 +128,42 @@ func main() {
 		}
 
 		for _, v := range nextResult.Value {
-			_ = v
+			vmScaleSetInfoMap[*v.Name] = VmScaleSetInfo{
+				Name:     *v.Name,
+				Location: *v.Location,
+				Capacity: int(*v.SKU.Capacity),
+				TypeSku:  *v.SKU.Name,
+			}
 		}
 	}
 
-	generatePricingMapFromApi()
-	fmt.Println("hello world")
+	machineTypes := map[string]bool{}
+	for _, info := range vmScaleSetInfoMap {
+		machineTypes[info.TypeSku] = true
+	}
+	machineTypeNames := []string{}
+	for i := range machineTypes {
+		machineTypeNames = append(machineTypeNames, i)
+	}
+
+	pm := generatePricingMapFromApi(machineTypeNames)
+
+	type TotalPriceInfo struct {
+		Info       VmScaleSetInfo
+		PriceTotal float32
+	}
+	totalPricesMap := map[string]TotalPriceInfo{}
+
+	for vmName, info := range vmScaleSetInfoMap {
+		pricePerType := pm[info.Location][info.TypeSku].RetailPrice
+		totalPrice := pricePerType * float32(info.Capacity)
+		totalPricesMap[vmName] = TotalPriceInfo{
+			Info:       info,
+			PriceTotal: totalPrice,
+		}
+	}
+
+	for name, info := range totalPricesMap {
+		fmt.Printf("VMSS %s, cost total: %v\n", name, info.PriceTotal)
+	}
 }
