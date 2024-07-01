6725204896f856173dae1a97475fa20f5037a36a
Logan Ballard <loganballard@gmail.com>
2024-07-01 16:04:18 -0400

    adding more tests

--- pkg/azure/aks/price_store_test.go
+++ pkg/azure/aks/price_store_test.go
@@ -1,6 +1,8 @@
 package aks
 
 import (
+	"log/slog"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,6 +12,89 @@ import (
 func TestPriceStoreMapCreation(t *testing.T) {
 	// TODO - mock
 	t.Skip()
+}
+
+func TestGetPriceInfoFromVmInfo(t *testing.T) {
+	fakePriceStore := &PriceStore{
+		RegionMap: map[string]PriceByPriority{
+			"region1": {
+				OnDemand: PriceByOperatingSystem{
+					Linux: PriceBySku{
+						"sku1": &retailPriceSdk.ResourceSKU{
+							RetailPrice: 1.0,
+						},
+					},
+				},
+			},
+		},
+		regionMapLock: &sync.RWMutex{},
+		logger:        slog.Default(),
+	}
+
+	testTable := map[string]struct {
+		vmInfo        *VirtualMachineInfo
+		expectedPrice float64
+		expectedErr   error
+	}{
+		"nil vm info": {
+			vmInfo:        nil,
+			expectedPrice: 0.0,
+			expectedErr:   ErrPriceInformationNotFound,
+		},
+
+		"missing region": {
+			vmInfo: &VirtualMachineInfo{
+				Priority:        OnDemand,
+				OperatingSystem: Linux,
+				MachineTypeSku:  "sku1",
+			},
+			expectedPrice: 0.0,
+			expectedErr:   ErrPriceInformationNotFound,
+		},
+		"missing sku": {
+			vmInfo: &VirtualMachineInfo{
+				Priority:        OnDemand,
+				Region:          "region1",
+				OperatingSystem: Linux,
+			},
+			expectedPrice: 0.0,
+			expectedErr:   ErrPriceInformationNotFound,
+		},
+		"all information complete": {
+			vmInfo: &VirtualMachineInfo{
+				Priority:        OnDemand,
+				Region:          "region1",
+				OperatingSystem: Linux,
+				MachineTypeSku:  "sku1",
+			},
+			expectedPrice: 1.0,
+			expectedErr:   nil,
+		},
+		"all information complete but not in map": {
+			vmInfo: &VirtualMachineInfo{
+				Priority:        OnDemand,
+				Region:          "region2",
+				OperatingSystem: Windows,
+				MachineTypeSku:  "sku3",
+			},
+			expectedPrice: 0.0,
+			expectedErr:   ErrPriceInformationNotFound,
+		},
+	}
+
+	for name, test := range testTable {
+		t.Run(name, func(t *testing.T) {
+			price, err := fakePriceStore.getPriceInfoFromVmInfo(test.vmInfo)
+
+			assert.Equal(t, test.expectedPrice, price)
+			if test.expectedErr != nil {
+				assert.Equal(t, test.expectedErr, err)
+			} else {
+				assert.Nil(t, err)
+			}
+
+		})
+	}
 }
 
 func TestDetermineMachineOperatingSystem(t *testing.T) {
