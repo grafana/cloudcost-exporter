7eadb10420eb13c981d2ab2041ffe4c9b1c2b675
gokul.menon <gokul.menon@free-now.com>
2024-10-30 20:43:19 +0100

    Added reusable GenerateDesc function across all providers

--- pkg/azure/aks/aks.go
+++ pkg/azure/aks/aks.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/grafana/cloudcost-exporter/pkg/utils"
 	"log/slog"
 	"time"
 
@@ -58,23 +59,26 @@ var (
 
 // Prometheus Metrics
 var (
-	InstanceCPUHourlyCostDesc = prometheus.NewDesc(
-		prometheus.BuildFQName(cloudcost_exporter.MetricPrefix, subsystem, "instance_cpu_usd_per_core_hour"),
-		"The cpu cost a compute instance in USD/(core*h)",
+	InstanceCPUHourlyCostDesc = utils.GenerateDesc(
+		cloudcost_exporter.MetricPrefix,
+		subsystem,
+		utils.InstanceCPUCostSuffix,
+		"The cpu cost a a compute instance in USD/(core*h)",
 		[]string{"instance", "region", "machine_type", "family", "cluster_name", "price_tier", "operating_system"},
-		nil,
 	)
-	InstanceMemoryHourlyCostDesc = prometheus.NewDesc(
-		prometheus.BuildFQName(cloudcost_exporter.MetricPrefix, subsystem, "instance_memory_usd_per_gib_hour"),
+	InstanceMemoryHourlyCostDesc = utils.GenerateDesc(
+		cloudcost_exporter.MetricPrefix,
+		subsystem,
+		utils.InstanceMemoryCostSuffix,
 		"The memory cost of a compute instance in USD/(GiB*h)",
 		[]string{"instance", "region", "machine_type", "family", "cluster_name", "price_tier", "operating_system"},
-		nil,
 	)
-	InstanceTotalHourlyCostDesc = prometheus.NewDesc(
-		prometheus.BuildFQName(cloudcost_exporter.MetricPrefix, subsystem, "instance_total_usd_per_hour"),
-		"The total cost of an compute instance in USD/h",
+	InstanceTotalHourlyCostDesc = utils.GenerateDesc(
+		cloudcost_exporter.MetricPrefix,
+		subsystem,
+		utils.InstanceTotalCostSuffix,
+		"The total cost of a compute instance in USD/h",
 		[]string{"instance", "region", "machine_type", "family", "cluster_name", "price_tier", "operating_system"},
-		nil,
 	)
 )
 
