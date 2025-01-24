c276166f29a0958107885b1e759670285d1681b0
Kévin Gomez <kevin.gomez@grafana.com>
2025-01-24 17:25:20 +0100

    Start cleaning up the results of the dashboard converter

--- config/grafana/operations_dashboard.go
+++ config/grafana/operations_dashboard.go
@@ -13,53 +13,64 @@ import (
 	"github.com/grafana/grafana-foundation-sdk/go/timeseries"
 )
 
+func prometheusDatasourceRef() dashboard.DataSourceRef {
+	return dashboard.DataSourceRef{
+		Type: cog.ToPtr[string]("prometheus"),
+		Uid:  cog.ToPtr[string]("${datasource}"),
+	}
+}
+
+func prometheusQuery(expression string, legendFormat string) *prometheus.DataqueryBuilder {
+	return prometheus.NewDataqueryBuilder().
+		Expr(expression).
+		Range().
+		LegendFormat(legendFormat)
+}
+
 func main() {
 	builder := dashboard.NewDashboardBuilder("CloudCost Exporter").
-		Id(10297).
+		// leaving this for BC reasons, but a proper human-readable UID would be better.
 		Uid("1a9c0de366458599246184cf0ae8b468").
-		Title("CloudCost Exporter").
 		Editable().
-		Tooltip(1).
-		Timepicker(dashboard.NewTimePickerBuilder()).
+		Tooltip(dashboard.DashboardCursorSyncCrosshair).
 		Refresh("30s").
-		Version(0x2).
-		Variables([]cog.Builder[dashboard.VariableModel]{dashboard.NewDatasourceVariableBuilder("datasource").
-			Name("datasource").
+		WithVariable(dashboard.NewDatasourceVariableBuilder("datasource").
 			Label("Data Source").
 			Type("prometheus"),
-			dashboard.NewQueryVariableBuilder("cluster").
-				Name("cluster").
-				Query(dashboard.StringOrMap{Map: map[string]interface{}{"query": "label_values(tanka_environment_info{app=\"cloudcost-exporter\"},exported_cluster)", "refId": "PrometheusVariableQueryEditor-VariableQuery"}}).
-				Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("${datasource}")}).
-				Current(dashboard.VariableOption{Text: dashboard.StringOrArrayOfString{ArrayOfString: []string{"dev-us-central-0", "dev-us-east-0", "prod-us-east-0"}}, Value: dashboard.StringOrArrayOfString{ArrayOfString: []string{"dev-us-central-0", "dev-us-east-0", "prod-us-east-0"}}}).
-				Multi(true).
-				Refresh(1).
-				IncludeAll(true).
-				AllValue(".*")}).
-		Annotations([]cog.Builder[dashboard.AnnotationQuery]{dashboard.NewAnnotationQueryBuilder().
+		).
+		WithVariable(dashboard.NewQueryVariableBuilder("cluster").
+			// TODO: this looks grafana-specific
+			Query(dashboard.StringOrMap{
+				String: cog.ToPtr("label_values(tanka_environment_info{app=\"cloudcost-exporter\"},exported_cluster)"),
+			}).
+			Datasource(prometheusDatasourceRef()).
+			Multi(true).
+			Refresh(dashboard.VariableRefreshOnDashboardLoad).
+			IncludeAll(true).
+			AllValue(".*"),
+		).
+		Annotation(dashboard.NewAnnotationQueryBuilder().
 			Name("Annotations & Alerts").
-			Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("grafana"), Uid: cog.ToPtr[string]("-- Grafana --")}).
+			Datasource(dashboard.DataSourceRef{
+				Type: cog.ToPtr[string]("grafana"),
+				Uid:  cog.ToPtr[string]("-- Grafana --"),
+			}).
 			Hide(true).
 			IconColor("rgba(0, 211, 255, 1)").
 			Type("dashboard").
-			BuiltIn(1)}).
-		Preload(false).
-		WithRow(dashboard.NewRowBuilder("Overview").
-			Title("Overview").
-			GridPos(dashboard.GridPos{H: 1, W: 24, X: 0, Y: 0}).
-			Id(0x8)).
-		WithPanel(buildUpPanel()).
-		WithPanel(buildCollectorScrapeDurationPanel()).
+			BuiltIn(1),
+		).
+		WithRow(dashboard.NewRowBuilder("Overview")).
+		WithPanel(collectorStatusCurrent().Height(6).Span(11)).
+		WithPanel(collectorScrapeDurationOverTime()).
 		WithRow(dashboard.NewRowBuilder("AWS").
 			Title("AWS").
-			GridPos(dashboard.GridPos{H: 1, W: 24, X: 0, Y: 15}).
-			Id(0x7)).
+			GridPos(dashboard.GridPos{H: 1, W: 24, X: 0, Y: 15})).
 		WithPanel(buildCostExplorerAPIRequestsPanel()).
 		WithPanel(buildAWSS3NextScrapePanel()).
 		WithRow(dashboard.NewRowBuilder("GCP").
 			Title("GCP").
-			GridPos(dashboard.GridPos{H: 1, W: 24, X: 0, Y: 24}).
-			Id(0x2)).
+			GridPos(dashboard.GridPos{H: 1, W: 24, X: 0, Y: 24})).
 		WithPanel(buildGCPlistBucketsRPSPanel()).
 		WithPanel(buildNextScrapePanel())
 
@@ -75,37 +86,27 @@ func main() {
 	fmt.Println(string(dashboardJson))
 }
 
-func buildCollectorScrapeDurationPanel() *timeseries.PanelBuilder {
+func collectorScrapeDurationOverTime() *timeseries.PanelBuilder {
 	return timeseries.NewPanelBuilder().
-		Id(0xd).
-		Targets([]cog.Builder[variants.Dataquery]{prometheus.NewDataqueryBuilder().
-			Expr("cloudcost_exporter_collector_last_scrape_duration_seconds").
-			Range().
-			EditorMode("code").
-			LegendFormat("{{provider}}:{{collector}}").
-			RefId("A").
-			Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("${datasource}")}),
-			prometheus.NewDataqueryBuilder().
-				Expr("vector(60)").
-				Range().
-				EditorMode("code").
-				LegendFormat("scrape_interval").
-				RefId("B").
-				Hide(false).
-				Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("${datasource}")})}).
 		Title("Collector Scrape Duration").
 		Description("Duration of scrapes by provider and collector").
-		Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("${datasource}")}).
+		Datasource(prometheusDatasourceRef()).
+		WithTarget(
+			prometheusQuery("cloudcost_exporter_collector_last_scrape_duration_seconds", "{{provider}}:{{collector}}"),
+		).
 		GridPos(dashboard.GridPos{H: 8, W: 24, X: 0, Y: 7}).
 		Height(0x8).
 		Span(0x18).
 		Unit("s").
+		ThresholdsStyle(common.NewGraphThresholdsStyleConfigBuilder().Mode(common.GraphThresholdsStyleModeLine)).
 		Thresholds(dashboard.NewThresholdsConfigBuilder().
-			Mode("absolute").
-			Steps([]dashboard.Threshold{dashboard.Threshold{Color: "green"},
-				dashboard.Threshold{Value: cog.ToPtr[float64](80), Color: "red"}})).
-		ColorScheme(dashboard.NewFieldColorBuilder().
-			Mode("palette-classic")).
+			Mode(dashboard.ThresholdsModeAbsolute).
+			Steps([]dashboard.Threshold{
+				{Color: "green"},
+				{Value: cog.ToPtr[float64](60), Color: "red"},
+			}),
+		).
+		ColorScheme(dashboard.NewFieldColorBuilder().Mode("palette-classic")).
 		Legend(common.NewVizLegendOptionsBuilder().
 			DisplayMode("table").
 			Placement("bottom").
@@ -120,8 +121,6 @@ func buildCollectorScrapeDurationPanel() *timeseries.PanelBuilder {
 			Sort("none")).
 		DrawStyle("line").
 		GradientMode("none").
-		ThresholdsStyle(common.NewGraphThresholdsStyleConfigBuilder().
-			Mode("off")).
 		LineWidth(1).
 		LineInterpolation("linear").
 		FillOpacity(0).
@@ -148,7 +147,6 @@ func buildCollectorScrapeDurationPanel() *timeseries.PanelBuilder {
 
 func buildCostExplorerAPIRequestsPanel() *timeseries.PanelBuilder {
 	return timeseries.NewPanelBuilder().
-		Id(0x6).
 		Targets([]cog.Builder[variants.Dataquery]{prometheus.NewDataqueryBuilder().
 			Expr("sum by (cluster) (increase(cloudcost_exporter_aws_s3_cost_api_requests_total{cluster=~\"$cluster\"}[5m]))").
 			Range().
@@ -157,7 +155,7 @@ func buildCostExplorerAPIRequestsPanel() *timeseries.PanelBuilder {
 			RefId("A").
 			Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("000000134")})}).
 		Title("CostExplorer API Requests").
-		Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("${datasource}")}).
+		Datasource(prometheusDatasourceRef()).
 		GridPos(dashboard.GridPos{H: 8, W: 11, X: 0, Y: 16}).
 		Height(0x8).
 		Span(0xb).
@@ -205,7 +203,6 @@ func buildCostExplorerAPIRequestsPanel() *timeseries.PanelBuilder {
 
 func buildAWSS3NextScrapePanel() *timeseries.PanelBuilder {
 	return timeseries.NewPanelBuilder().
-		Id(0x9).
 		Targets([]cog.Builder[variants.Dataquery]{prometheus.NewDataqueryBuilder().
 			Expr("max by (cluster) (cloudcost_exporter_aws_s3_next_scrape{cluster=~\"$cluster\"}) - time() ").
 			Range().
@@ -215,7 +212,7 @@ func buildAWSS3NextScrapePanel() *timeseries.PanelBuilder {
 			Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("000000134")})}).
 		Title("Next pricing map refresh").
 		Description("The AWS s3 module uses cost data pulled from Cost Explorer, which costs $0.01 per API call. The cost metrics are refreshed every hour, so if this value goes below 0, it indicates a problem with refreshing the pricing map and thus needs investigation.").
-		Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("${datasource}")}).
+		Datasource(prometheusDatasourceRef()).
 		GridPos(dashboard.GridPos{H: 8, W: 13, X: 11, Y: 16}).
 		Height(0x8).
 		Span(0xd).
@@ -263,7 +260,6 @@ func buildAWSS3NextScrapePanel() *timeseries.PanelBuilder {
 
 func buildGCPlistBucketsRPSPanel() *timeseries.PanelBuilder {
 	return timeseries.NewPanelBuilder().
-		Id(0x3).
 		Targets([]cog.Builder[variants.Dataquery]{prometheus.NewDataqueryBuilder().
 			Expr("sum by (cluster, status) (increase(cloudcost_exporter_gcp_gcs_bucket_list_status_total[5m]))").
 			Range().
@@ -272,7 +268,7 @@ func buildGCPlistBucketsRPSPanel() *timeseries.PanelBuilder {
 			RefId("A").
 			Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("000000134")})}).
 		Title("GCS List Buckets Requests Per Second").
-		Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("${datasource}")}).
+		Datasource(prometheusDatasourceRef()).
 		GridPos(dashboard.GridPos{H: 7, W: 11, X: 0, Y: 25}).
 		Height(0x7).
 		Span(0xb).
@@ -320,7 +316,6 @@ func buildGCPlistBucketsRPSPanel() *timeseries.PanelBuilder {
 
 func buildNextScrapePanel() *timeseries.PanelBuilder {
 	return timeseries.NewPanelBuilder().
-		Id(0xa).
 		Targets([]cog.Builder[variants.Dataquery]{prometheus.NewDataqueryBuilder().
 			Expr("max by (cluster) (cloudcost_exporter_gcp_gcs_next_scrape{cluster=~\"$cluster\"}) - time() ").
 			Range().
@@ -330,7 +325,7 @@ func buildNextScrapePanel() *timeseries.PanelBuilder {
 			Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("000000134")})}).
 		Title("GCS Pricing Map Refresh Time").
 		Description("The amount of time before the next refresh of the GCS pricing map. ").
-		Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("${datasource}")}).
+		Datasource(prometheusDatasourceRef()).
 		GridPos(dashboard.GridPos{H: 7, W: 13, X: 11, Y: 25}).
 		Height(0x7).
 		Span(0xd).
@@ -376,38 +371,44 @@ func buildNextScrapePanel() *timeseries.PanelBuilder {
 		AxisBorderShow(false)
 }
 
-func buildUpPanel() *stat.PanelBuilder {
+func collectorStatusCurrent() *stat.PanelBuilder {
 	return stat.NewPanelBuilder().
-		Id(0xc).
-		Targets([]cog.Builder[variants.Dataquery]{prometheus.NewDataqueryBuilder().
-			Expr("max by (provider, collector) (cloudcost_exporter_collector_last_scrape_error == 0)").
-			Range().
-			EditorMode("code").
-			LegendFormat("{{provider}}:{{collector}}").
-			RefId("A").
-			Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("${datasource}")})}).
 		Title("Collector Status").
 		Description("Display the status of all the collectors running.").
-		Datasource(dashboard.DataSourceRef{Type: cog.ToPtr[string]("prometheus"), Uid: cog.ToPtr[string]("${datasource}")}).
-		GridPos(dashboard.GridPos{H: 6, W: 11, X: 0, Y: 1}).
-		Height(0x6).
-		Span(0xb).
+		Datasource(prometheusDatasourceRef()).
 		Unit("short").
-		Mappings([]dashboard.ValueMapping{dashboard.ValueMapOrRangeMapOrRegexMapOrSpecialValueMap{ValueMap: cog.ToPtr[dashboard.ValueMap](dashboard.ValueMap{Type: "value", Options: map[string]dashboard.ValueMappingResult{"0": dashboard.ValueMappingResult{Text: cog.ToPtr[string]("Up"), Index: cog.ToPtr[int32](0)}}})},
-			dashboard.ValueMapOrRangeMapOrRegexMapOrSpecialValueMap{SpecialValueMap: cog.ToPtr[dashboard.SpecialValueMap](dashboard.SpecialValueMap{Type: "special", Options: dashboard.DashboardSpecialValueMapOptions{Match: "null+nan", Result: dashboard.ValueMappingResult{Text: cog.ToPtr[string]("Down"), Color: cog.ToPtr[string]("red"), Index: cog.ToPtr[int32](1)}}})}}).
-		Thresholds(dashboard.NewThresholdsConfigBuilder().
-			Mode("absolute").
-			Steps([]dashboard.Threshold{dashboard.Threshold{Color: "green"},
-				dashboard.Threshold{Value: cog.ToPtr[float64](80), Color: "red"}})).
-		ColorScheme(dashboard.NewFieldColorBuilder().
-			Mode("thresholds")).
-		GraphMode("area").
-		ColorMode("value").
-		JustifyMode("auto").
-		TextMode("auto").
+		WithTarget(
+			prometheusQuery("max by (provider, collector) (cloudcost_exporter_collector_last_scrape_error == 0)", "{{provider}}:{{collector}}").
+				Instant(),
+		).
+		JustifyMode(common.BigValueJustifyModeAuto).
+		TextMode(common.BigValueTextModeAuto).
+		Orientation(common.VizOrientationAuto).
 		ReduceOptions(common.NewReduceDataOptionsBuilder().
 			Values(false).
-			Calcs([]string{"lastNotNull"})).
-		PercentChangeColorMode("standard").
-		Orientation("auto")
+			Calcs([]string{"lastNotNull"}),
+		).
+		Mappings([]dashboard.ValueMapping{
+			{
+				ValueMap: cog.ToPtr[dashboard.ValueMap](dashboard.ValueMap{
+					Type: "value",
+					Options: map[string]dashboard.ValueMappingResult{
+						"0": {Text: cog.ToPtr[string]("Up"), Index: cog.ToPtr[int32](0)},
+					},
+				}),
+			},
+			{
+				SpecialValueMap: cog.ToPtr[dashboard.SpecialValueMap](dashboard.SpecialValueMap{
+					Type: "special",
+					Options: dashboard.DashboardSpecialValueMapOptions{
+						Match: "null+nan",
+						Result: dashboard.ValueMappingResult{
+							Text:  cog.ToPtr[string]("Down"),
+							Color: cog.ToPtr[string]("red"),
+							Index: cog.ToPtr[int32](1),
+						},
+					},
+				}),
+			},
+		})
 }
