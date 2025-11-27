68367d2c0aa739e43bc6f418f3d9a692aec02594
nikimanoledaki <niki.manoledaki@grafana.com>
2025-11-27 14:36:07 +0100

    Convert error-level log to warning-level log to reduce log volume
    Unnecessary log volume being caused by this err-level log that should
    be a warning. We converted it for other EC2 instances, let's do the same
    for spots.

--- pkg/aws/ec2/pricing_map.go
+++ pkg/aws/ec2/pricing_map.go
@@ -146,8 +146,15 @@ func (cpm *ComputePricingMap) GenerateComputePricingMap(ondemandPrices []string,
 		}
 		err = cpm.AddToComputePricingMap(price, spotProductTerm)
 		if err != nil {
-			cpm.logger.Error(fmt.Sprintf("error adding to pricing map: %s", err))
-			continue
+			switch {
+			case errors.Is(err, ErrInstanceTypeAlreadyExists):
+				// Only warn in debug mode about this error, since we only want one price per instance type
+				cpm.logger.Debug(fmt.Sprintf("skipping addition to pricing map: %s", err))
+				continue
+			default:
+				cpm.logger.Error(fmt.Sprintf("error adding to pricing map: %s", err))
+				continue
+			}
 		}
 	}
 	return nil
